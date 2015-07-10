f019948973b40db33ba107192183695f51ff1d64
PJ Passalacqua <pjpassa@gmail.com>
2015-07-10 15:52:56 -0400

    updated model so state has choices

--- restaurant/orders/models.py
+++ restaurant/orders/models.py
@@ -22,10 +22,61 @@ class MenuItem(models.Model):
 
 
 class Address(models.Model):
+    STATE_CHOICES = (('AL','Alabama'),
+                    ('AK','Alaska'),
+                    ('AZ','Arizona'),
+                    ('AR','Arkansas'),
+                    ('CA','California'),
+                    ('CO','Colorado'),
+                    ('CT','Connecticut'),
+                    ('DE','Delaware'),
+                    ('DC','District of Columbia'),
+                    ('FL','Florida'),
+                    ('GA','Georgia'),
+                    ('HI','Hawaii'),
+                    ('ID','Idaho'),
+                    ('IL','Illinois'),
+                    ('IN','Indiana'),
+                    ('IA','Iowa'),
+                    ('KS','Kansas'),
+                    ('KY','Kentucky'),
+                    ('LA','Louisiana'),
+                    ('ME','Maine'),
+                    ('MD','Maryland'),
+                    ('MA','Massachusetts'),
+                    ('MI','Michigan'),
+                    ('MN','Minnesota'),
+                    ('MS','Mississippi'),
+                    ('MO','Missouri'),
+                    ('MT','Montana'),
+                    ('NE','Nebraska'),
+                    ('NV','Nevada'),
+                    ('NH','New Hampshire'),
+                    ('NJ','New Jersey'),
+                    ('NM','New Mexico'),
+                    ('NY','New York'),
+                    ('NC','North Carolina'),
+                    ('ND','North Dakota'),
+                    ('OH','Ohio'),
+                    ('OK','Oklahoma'),
+                    ('OR','Oregon'),
+                    ('PA','Pennsylvania'),
+                    ('RI','Rhode Island'),
+                    ('SC','South Carolina'),
+                    ('SD','South Dakota'),
+                    ('TN','Tennessee'),
+                    ('TX','Texas'),
+                    ('UT','Utah'),
+                    ('VT','Vermont'),
+                    ('VA','Virginia'),
+                    ('WA','Washington'),
+                    ('WV','West Virginia'),
+                    ('WI','Wisconsin'),
+                    ('WY','Wyoming'))
     line_1 = models.CharField(max_length=100)
     line_2 = models.CharField(max_length=100, blank=True)
     city = models.CharField(max_length=20)
-    state = models.CharField(max_length=2)
+    state = models.CharField(max_length=2, choices=STATE_CHOICES, blank=True, default='AL')
     zipcode = models.CharField(max_length=5)
     plus_4 = models.CharField(max_length=4, blank=True)
 
