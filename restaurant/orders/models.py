1b4e68311de5f5dca5f2d4bc78be42af00ca1b1a
SorenOlegnowicz <soren.olegnowicz@gmail.com>
2015-07-10 13:08:29 -0400

    menu choices

--- restaurant/orders/models.py
+++ restaurant/orders/models.py
@@ -5,15 +5,27 @@ from django.db import models
 # Create your models here.
 
 class MenuItem(models.Model):
+    APPETIZER = 'App'
+    ENTREE = 'Ent'
+    DESERT = 'Des'
+    DRINK = 'Dri'
+    MENU_CHOICES = (
+        (APPETIZER, 'Appetizer'),
+        (ENTREE, 'Entree'),
+        (DESERT, 'Desert'),
+        (DRINK, 'Drink')
+    )
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=100, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    menu_parts = models.CharField(max_length=20, choices=MENU_CHOICES)
 
 
 class Address(models.Model):
     line_1 = models.CharField(max_length=100)
     line_2 = models.CharField(max_length=100)
     city = models.CharField(max_length=20)
+    state = models.CharField(max_length=2)
     zipcode = models.CharField(max_length=5)
     plus_4 = models.CharField(max_length=4)
 
